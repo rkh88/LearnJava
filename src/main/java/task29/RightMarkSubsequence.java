0c69582d9cd0d82d08ff695fdf6903f3bcd65602
rkh88 <ruslan.khatipov@gmail.com>
2021-07-05 01:03:17 +0300

    task 27

--- src/main/java/task29/RightMarkSubsequence.java
+++ src/main/java/task29/RightMarkSubsequence.java
@@ -1,21 +1,21 @@
 package task29;
-
+import task25.*;
 import task27.*;
 
 public class RightMarkSubsequence {
 
     Stack<Character> st = new ListStack<Character>();
-    String s1 = "{";
-    String s2 = "(";
-    String s3 = "[";
-    String s11 = "}";
-    String s22 = ")";
-    String s33 = "]";
+    private String s1 = "{";
+    private String s2 = "(";
+    private String s3 = "[";
+    private String s11 = "}";
+    private String s22 = ")";
+    private String s33 = "]";
 
     public boolean markProcedure(String s){
         for (int i = 0; i < s.length(); i++) {
-            if(st.isEmpty == false){
-                if (charAt(i).equals(s11) && st.peekLast.equals(s1) || charAt(i).equals(s22) && st.peekLast.equals(s2) || charAt(i).equals(s33) && st.peekLast.equals(s3)){
+            if(st.isEmpty() == false){
+                if (charAt(i).equals(s11) && st.peekLast().equals(s1) || charAt(i).equals(s22) && st.peekLast().equals(s2) || charAt(i).equals(s33) && st.peekLast().equals(s3)){
                     st.pollLast();
                 }
             }
